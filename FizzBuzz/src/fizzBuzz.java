5753561eaa597cb97416d1f8b49cdd6a3bcd6d1f
emily.deline <emily.deline@MU-C1-04.mullen.pvt.k12.co.us>
2019-08-28 11:41:41 -0600

    First commit of FizzBuzz

---
+++ FizzBuzz/src/fizzBuzz.java
@@ -0,0 +1,28 @@
+
+public class fizzBuzz
+	{
+		static int num;
+		public static void main(String[] args)
+			{
+				for (num = 1; num <= 100; num ++)
+				{
+					if (num % 3 == 0)
+						{
+							System.out.println("Fizz");
+						}
+					else if (num % 5 == 0)
+						{
+							System.out.println("Buzz");
+						}
+					else if (num % 3 == 0 && num % 5 == 0)
+						{
+							System.out.println("FizzBuzz");
+						}
+					else 
+						{
+							System.out.println(num);
+						}
+				}
+
+			}
+	}
